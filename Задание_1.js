d29445846deeff224b646087a530442dcb1bb220
LudmilaMihailovna <109368288+LudmilaMihailovna@users.noreply.github.com>
2022-11-07 21:04:14 +0700

    Create Задание_1.js

---
+++ Задание_1.js
@@ -0,0 +1,10 @@
+let num = +prompt('Введите число');
+if (isNaN (num)) {
+  console.log ('Упс, кажется, вы ошиблись');
+} else {
+  if (num % 2 == 0) {
+  console.log('число четное');
+} else {
+  console.log('число нечетное');
+} 
+}
